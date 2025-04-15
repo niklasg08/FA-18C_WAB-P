ccf8309f98adae2e6ca06ffecc5a8f5b7472aef8
Niklas Glienke <niklas.glienke@icloud.com>
2025-04-15 15:00:20 +0200

    Renamed Application and unnecessary comments deleted

---
+++ FA-18C_WAB-P/FA-18C_WAB-P/Program.cs
@@ -0,0 +1,15 @@
+namespace FA_18C_WAB_P
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Main());
+        }
+    }
+}
