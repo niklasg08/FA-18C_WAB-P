8ce870f9936ab0202b9e5244f204a02c8d184c74
Niklas Glienke <niklas.glienke@icloud.com>
2025-04-15 17:45:25 +0200

    Added left Panel without Logic

--- FA-18C WAB-P/Main.Designer.cs
+++ FA-18C WAB-P/Main.Designer.cs
@@ -31,23 +31,33 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             comboBox_Station9 = new ComboBox();
             splitContainer1 = new SplitContainer();
+            trackBar_Fuel = new TrackBar();
+            checkBox_AfterburnerTO = new CheckBox();
+            textBox_Ammo = new TextBox();
+            textBox_Fuel = new TextBox();
+            label_Ammo = new Label();
+            label_Fuel = new Label();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
             panel1 = new Panel();
-            comboBox_Station1 = new ComboBox();
-            comboBox_Station8 = new ComboBox();
-            comboBox_Station7 = new ComboBox();
-            comboBox_Station6 = new ComboBox();
-            comboBox_Station3 = new ComboBox();
-            comboBox_Station5 = new ComboBox();
-            comboBox_Station4 = new ComboBox();
             comboBox_Station2 = new ComboBox();
+            comboBox_Station4 = new ComboBox();
+            comboBox_Station5 = new ComboBox();
+            comboBox_Station3 = new ComboBox();
+            comboBox_Station6 = new ComboBox();
+            comboBox_Station7 = new ComboBox();
+            comboBox_Station8 = new ComboBox();
+            comboBox_Station1 = new ComboBox();
+            trackBar_Ammo = new TrackBar();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
             splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar_Fuel).BeginInit();
             tabControl1.SuspendLayout();
             panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)trackBar_Ammo).BeginInit();
             SuspendLayout();
             // 
             // comboBox_Station9
@@ -68,6 +78,13 @@
             // splitContainer1.Panel1
             // 
             splitContainer1.Panel1.BackColor = SystemColors.ActiveBorder;
+            splitContainer1.Panel1.Controls.Add(trackBar_Ammo);
+            splitContainer1.Panel1.Controls.Add(trackBar_Fuel);
+            splitContainer1.Panel1.Controls.Add(checkBox_AfterburnerTO);
+            splitContainer1.Panel1.Controls.Add(textBox_Ammo);
+            splitContainer1.Panel1.Controls.Add(textBox_Fuel);
+            splitContainer1.Panel1.Controls.Add(label_Ammo);
+            splitContainer1.Panel1.Controls.Add(label_Fuel);
             splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
             // 
             // splitContainer1.Panel2
@@ -78,6 +95,58 @@
             splitContainer1.Size = new Size(1456, 359);
             splitContainer1.SplitterDistance = 728;
             splitContainer1.TabIndex = 6;
+            // 
+            // trackBar_Fuel
+            // 
+            trackBar_Fuel.Location = new Point(22, 71);
+            trackBar_Fuel.Maximum = 100;
+            trackBar_Fuel.Name = "trackBar_Fuel";
+            trackBar_Fuel.Size = new Size(683, 69);
+            trackBar_Fuel.TabIndex = 5;
+            trackBar_Fuel.Value = 100;
+            // 
+            // checkBox_AfterburnerTO
+            // 
+            checkBox_AfterburnerTO.AutoSize = true;
+            checkBox_AfterburnerTO.Location = new Point(22, 307);
+            checkBox_AfterburnerTO.Name = "checkBox_AfterburnerTO";
+            checkBox_AfterburnerTO.Size = new Size(191, 29);
+            checkBox_AfterburnerTO.TabIndex = 4;
+            checkBox_AfterburnerTO.Text = "Afterburner Takeoff";
+            checkBox_AfterburnerTO.UseVisualStyleBackColor = true;
+            checkBox_AfterburnerTO.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // textBox_Ammo
+            // 
+            textBox_Ammo.Location = new Point(115, 156);
+            textBox_Ammo.Name = "textBox_Ammo";
+            textBox_Ammo.Size = new Size(150, 31);
+            textBox_Ammo.TabIndex = 3;
+            // 
+            // textBox_Fuel
+            // 
+            textBox_Fuel.Location = new Point(115, 34);
+            textBox_Fuel.Name = "textBox_Fuel";
+            textBox_Fuel.Size = new Size(150, 31);
+            textBox_Fuel.TabIndex = 2;
+            // 
+            // label_Ammo
+            // 
+            label_Ammo.AutoSize = true;
+            label_Ammo.Location = new Point(22, 156);
+            label_Ammo.Name = "label_Ammo";
+            label_Ammo.Size = new Size(71, 25);
+            label_Ammo.TabIndex = 1;
+            label_Ammo.Text = "Ammo:";
+            // 
+            // label_Fuel
+            // 
+            label_Fuel.AutoSize = true;
+            label_Fuel.Location = new Point(22, 34);
+            label_Fuel.Name = "label_Fuel";
+            label_Fuel.Size = new Size(87, 25);
+            label_Fuel.TabIndex = 0;
+            label_Fuel.Text = "Fuel in %:";
             // 
             // tabControl1
             // 
@@ -127,65 +196,15 @@
             panel1.Size = new Size(1456, 557);
             panel1.TabIndex = 7;
             // 
-            // comboBox_Station1
+            // comboBox_Station2
             // 
-            comboBox_Station1.FormattingEnabled = true;
-            comboBox_Station1.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station1.Location = new Point(1260, 185);
-            comboBox_Station1.Name = "comboBox_Station1";
-            comboBox_Station1.Size = new Size(182, 33);
-            comboBox_Station1.TabIndex = 6;
-            comboBox_Station1.Text = "Station 1";
-            // 
-            // comboBox_Station8
-            // 
-            comboBox_Station8.FormattingEnabled = true;
-            comboBox_Station8.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station8.Location = new Point(145, 296);
-            comboBox_Station8.Name = "comboBox_Station8";
-            comboBox_Station8.Size = new Size(182, 33);
-            comboBox_Station8.TabIndex = 7;
-            comboBox_Station8.Text = "Station 8";
-            // 
-            // comboBox_Station7
-            // 
-            comboBox_Station7.FormattingEnabled = true;
-            comboBox_Station7.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station7.Location = new Point(308, 391);
-            comboBox_Station7.Name = "comboBox_Station7";
-            comboBox_Station7.Size = new Size(182, 33);
-            comboBox_Station7.TabIndex = 8;
-            comboBox_Station7.Text = "Station 7";
-            // 
-            // comboBox_Station6
-            // 
-            comboBox_Station6.FormattingEnabled = true;
-            comboBox_Station6.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station6.Location = new Point(477, 492);
-            comboBox_Station6.Name = "comboBox_Station6";
-            comboBox_Station6.Size = new Size(182, 33);
-            comboBox_Station6.TabIndex = 9;
-            comboBox_Station6.Text = "Station 6";
-            // 
-            // comboBox_Station3
-            // 
-            comboBox_Station3.FormattingEnabled = true;
-            comboBox_Station3.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station3.Location = new Point(987, 391);
-            comboBox_Station3.Name = "comboBox_Station3";
-            comboBox_Station3.Size = new Size(182, 33);
-            comboBox_Station3.TabIndex = 10;
-            comboBox_Station3.Text = "Station 3";
-            // 
-            // comboBox_Station5
-            // 
-            comboBox_Station5.FormattingEnabled = true;
-            comboBox_Station5.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station5.Location = new Point(637, 310);
-            comboBox_Station5.Name = "comboBox_Station5";
-            comboBox_Station5.Size = new Size(182, 33);
-            comboBox_Station5.TabIndex = 10;
-            comboBox_Station5.Text = "Station 5";
+            comboBox_Station2.FormattingEnabled = true;
+            comboBox_Station2.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station2.Location = new Point(1153, 296);
+            comboBox_Station2.Name = "comboBox_Station2";
+            comboBox_Station2.Size = new Size(182, 33);
+            comboBox_Station2.TabIndex = 12;
+            comboBox_Station2.Text = "Station 2";
             // 
             // comboBox_Station4
             // 
@@ -197,15 +216,74 @@
             comboBox_Station4.TabIndex = 11;
             comboBox_Station4.Text = "Station 4";
             // 
-            // comboBox_Station2
+            // comboBox_Station5
             // 
-            comboBox_Station2.FormattingEnabled = true;
-            comboBox_Station2.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
-            comboBox_Station2.Location = new Point(1153, 296);
-            comboBox_Station2.Name = "comboBox_Station2";
-            comboBox_Station2.Size = new Size(182, 33);
-            comboBox_Station2.TabIndex = 12;
-            comboBox_Station2.Text = "Station 2";
+            comboBox_Station5.FormattingEnabled = true;
+            comboBox_Station5.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station5.Location = new Point(637, 310);
+            comboBox_Station5.Name = "comboBox_Station5";
+            comboBox_Station5.Size = new Size(182, 33);
+            comboBox_Station5.TabIndex = 10;
+            comboBox_Station5.Text = "Station 5";
+            // 
+            // comboBox_Station3
+            // 
+            comboBox_Station3.FormattingEnabled = true;
+            comboBox_Station3.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station3.Location = new Point(987, 391);
+            comboBox_Station3.Name = "comboBox_Station3";
+            comboBox_Station3.Size = new Size(182, 33);
+            comboBox_Station3.TabIndex = 10;
+            comboBox_Station3.Text = "Station 3";
+            // 
+            // comboBox_Station6
+            // 
+            comboBox_Station6.FormattingEnabled = true;
+            comboBox_Station6.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station6.Location = new Point(477, 492);
+            comboBox_Station6.Name = "comboBox_Station6";
+            comboBox_Station6.Size = new Size(182, 33);
+            comboBox_Station6.TabIndex = 9;
+            comboBox_Station6.Text = "Station 6";
+            // 
+            // comboBox_Station7
+            // 
+            comboBox_Station7.FormattingEnabled = true;
+            comboBox_Station7.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station7.Location = new Point(308, 391);
+            comboBox_Station7.Name = "comboBox_Station7";
+            comboBox_Station7.Size = new Size(182, 33);
+            comboBox_Station7.TabIndex = 8;
+            comboBox_Station7.Text = "Station 7";
+            // 
+            // comboBox_Station8
+            // 
+            comboBox_Station8.FormattingEnabled = true;
+            comboBox_Station8.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station8.Location = new Point(145, 296);
+            comboBox_Station8.Name = "comboBox_Station8";
+            comboBox_Station8.Size = new Size(182, 33);
+            comboBox_Station8.TabIndex = 7;
+            comboBox_Station8.Text = "Station 8";
+            // 
+            // comboBox_Station1
+            // 
+            comboBox_Station1.FormattingEnabled = true;
+            comboBox_Station1.Items.AddRange(new object[] { "Empty", "AIM-9L", "AIM-9M", "AIM-9X", "TCMI Pod" });
+            comboBox_Station1.Location = new Point(1260, 185);
+            comboBox_Station1.Name = "comboBox_Station1";
+            comboBox_Station1.Size = new Size(182, 33);
+            comboBox_Station1.TabIndex = 6;
+            comboBox_Station1.Text = "Station 1";
+            // 
+            // trackBar_Ammo
+            // 
+            trackBar_Ammo.Location = new Point(22, 193);
+            trackBar_Ammo.Maximum = 578;
+            trackBar_Ammo.Name = "trackBar_Ammo";
+            trackBar_Ammo.Size = new Size(683, 69);
+            trackBar_Ammo.TabIndex = 6;
+            trackBar_Ammo.Value = 578;
             // 
             // Main
             // 
@@ -220,11 +298,15 @@
             Name = "Main";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "F/A-18C Weight and Balance / Performance Calculator";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
             splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)trackBar_Fuel).EndInit();
             tabControl1.ResumeLayout(false);
             panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)trackBar_Ammo).EndInit();
             ResumeLayout(false);
         }
 
@@ -243,5 +325,12 @@
         private ComboBox comboBox_Station5;
         private ComboBox comboBox_Station3;
         private ComboBox comboBox_Station2;
+        private Label label_Fuel;
+        private Label label_Ammo;
+        private TextBox textBox_Fuel;
+        private TextBox textBox_Ammo;
+        private CheckBox checkBox_AfterburnerTO;
+        private TrackBar trackBar_Fuel;
+        private TrackBar trackBar_Ammo;
     }
 }
--- FA-18C WAB-P/Main.cs
+++ FA-18C WAB-P/Main.cs
@@ -5,18 +5,10 @@ namespace FA_18C_WAB_P
         public Main()
         {
             InitializeComponent();
+
+            textBox_Ammo.Text = "578";
+            textBox_Fuel.Text = "100";
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
         private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
         {
 
@@ -25,6 +17,27 @@ namespace FA_18C_WAB_P
         private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar_Fuel_ValueChanged()
+        {
+            label_Fuel.Text = trackBar_Fuel.Value.ToString();
+        }
+
+        private void textBox_Fuel_TextChanged()
+        {
+            if (int.TryParse(textBox_Fuel.Text, out int value))
+            {
+                if (value >= trackBar_Fuel.Minimum && value <= trackBar_Fuel.Maximum)
+                {
+                    trackBar_Fuel.Value = value;
+                }
+            }
         }
     }
 }
